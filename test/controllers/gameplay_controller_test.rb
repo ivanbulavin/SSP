6eba4155e13e1b5d4705644b1fba1285e5d9d7ad
Ivan <ivan.bulavin@gmail.com>
2015-03-26 11:30:39 +0200

    Revert "initial commit"
    This reverts commit 793b34787d856b499cba5d871eac9699a8f22de1.

--- test/controllers/gameplay_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GameplayControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/gameplay_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class GameplayHelperTest < ActionView::TestCase
-end
